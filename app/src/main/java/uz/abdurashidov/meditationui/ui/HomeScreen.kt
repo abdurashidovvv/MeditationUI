4ae0b7cf7157d2e01ff07f4d7ece717c398fb7ce
abdurashidovvv <abdurashidovsaydullo35@gmail.com>
2024-03-03 13:52:55 +0000

    3.0 ChipSection

--- app/src/main/java/uz/abdurashidov/meditationui/ui/HomeScreen.kt
+++ app/src/main/java/uz/abdurashidov/meditationui/ui/HomeScreen.kt
@@ -1,6 +1,7 @@
 package uz.abdurashidov.meditationui.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,17 +10,27 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import uz.abdurashidov.meditationui.R
+import uz.abdurashidov.meditationui.ui.theme.ButtonBlue
+import uz.abdurashidov.meditationui.ui.theme.DarkerButtonBlue
 import uz.abdurashidov.meditationui.ui.theme.DeepBlue
+import uz.abdurashidov.meditationui.ui.theme.TextWhite
 
 @Composable
 fun HomeScreen() {
@@ -30,6 +41,7 @@ fun HomeScreen() {
     ) {
         Column {
             GreetingSection()
+            ChipSection(chips = listOf("Sweet Sleep", "Insomnia", "Depression"))
         }
     }
 }
@@ -49,12 +61,10 @@ fun GreetingSection(
             verticalArrangement = Arrangement.Center
         ) {
             Text(
-                text = "Good morning, $name",
-                style = MaterialTheme.typography.headlineMedium
+                text = "Good morning, $name", style = MaterialTheme.typography.headlineMedium
             )
             Text(
-                text = "We wish you have a good day!",
-                style = MaterialTheme.typography.bodyMedium
+                text = "We wish you have a good day!", style = MaterialTheme.typography.bodyMedium
             )
         }
 
@@ -66,3 +76,28 @@ fun GreetingSection(
         )
     }
 }
+
+
+@Composable
+fun ChipSection(
+    chips: List<String>
+) {
+    var selectedChipIndex by remember {
+        mutableStateOf(0)
+    }
+
+    LazyRow() {
+        items(chips.size) {
+            Box(contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .padding(start = 15.dp, bottom = 15.dp, top = 15.dp)
+                    .clickable { selectedChipIndex = it }
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(if (selectedChipIndex == it) ButtonBlue else DarkerButtonBlue)
+                    .padding(15.dp)) {
+                Text(text = chips[it], color = TextWhite)
+            }
+        }
+    }
+}
+
